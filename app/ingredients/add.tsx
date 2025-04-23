ddbc056efae5c8f845f83410237728245e346819
Tony <93945730+TonyNgyen@users.noreply.github.com>
2025-04-22 22:14:26 -0700

    Added paths for adding ingredients and recipes

---
+++ app/ingredients/add.tsx
@@ -0,0 +1,29 @@
+import { Stack } from 'expo-router';
+import { useEffect, useState } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import IngredientListItem from '~/components/IngredientListItem';
+import { useAuth } from '~/contexts/AuthProvider';
+import { fetchUserIngredients } from '~/lib/ingredient';
+
+export default function AddIngredient() {
+  const [ingredients, setIngredients] = useState([]);
+  const { user } = useAuth();
+  useEffect(() => {
+    const fetch = async () => {
+      const ingredientsFetched = await fetchUserIngredients(user?.id);
+      setIngredients(ingredientsFetched);
+    };
+    fetch();
+  }, []);
+  return (
+    <View className="p-4">
+      <Stack.Screen options={{ title: 'Add Ingredient' }} />
+      <FlatList
+        className=""
+        data={ingredients}
+        renderItem={({ item }) => <IngredientListItem ingredient={item} />}
+        contentContainerStyle={{ gap: 10 }}
+      />
+    </View>
+  );
+}

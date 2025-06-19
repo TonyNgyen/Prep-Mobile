85c7176ff36277a6f0b727debe4676ae5b338621
Tony <93945730+TonyNgyen@users.noreply.github.com>
2025-06-18 22:05:25 -0700

    Removing unnecessary imports

--- components/LogIngredientInfo.tsx
+++ components/LogIngredientInfo.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, ScrollView, FlatList } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import { Ingredient, InventoryIngredient, InventoryRecipe, NutritionFacts } from '~/types';
 import { Feather } from '@expo/vector-icons';
 import { NUTRITIONAL_KEYS } from '~/constants/NUTRITIONAL_KEYS';
